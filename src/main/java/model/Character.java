21204df733e708768a04d510997bf8afcff61e1a
nellyiriberri <nellyiriberri@gmail.com>
2020-06-03 15:50:20 +0200

    Change id to identity generation

--- src/main/java/model/Character.java
+++ src/main/java/model/Character.java
@@ -16,7 +16,7 @@ import java.util.List;
 public abstract class Character {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name="name")
